e5d7f4e9ccec4728d449a610321878b916120829
Rabih El-Zahr <rabih.elzahr@icloud.com>
2024-11-06 01:31:02 -0500

    Add chackra-ui library and built basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Flex, Text, Input, Stack, Button } from "@chakra-ui/react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Flex height="100vh" width="100%" justifyContent="center">
+      <Flex flexDir="column" alignContent="center" justify="center">
+        <Text fontWeight="bold" fontSize={32} marginBottom={4}>Tax Calculator</Text>
+        <Stack>
+          <Input placeholder="Annual income" variant="outline" />
+          <Input placeholder="Tax year" variant="outline" />
+        </Stack>
+        <Button marginTop="4" borderRadius="8px">Submit</Button>
+      </Flex>
+    </Flex>
   )
 }
 
